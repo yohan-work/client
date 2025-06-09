ffac22a1b021cce7bc6c7ad885f0e9e5ef867a1d
Yohan.choi <yohanchoi.work@gmail.com>
2025-06-09 19:20:45 +0900

    add : final / comment mod,del .. etc
    1. 2줄 이상 댓글 작성 시 toggle 동작(class add/remove)
    2. 본인이 작성한 댓글만 수정/삭제 아이콘 표시
    3. 본인이 작성한 댓글에 대해 수정/삭제 가능
    4. 댓글 수정 시 수정/삭제 아이콘 hide. 취소 및 저장 시 show
    5. 댓글 내용 변경이 없으면 저장 버튼 비활성화

--- w01/src/components/post/ReplyList.jsx
+++ w01/src/components/post/ReplyList.jsx
@@ -7,10 +7,17 @@ import {
   orderBy,
   where,
   onSnapshot,
+  doc,
+  updateDoc,
+  deleteDoc,
 } from "firebase/firestore";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Button, Form } from "react-bootstrap";
+import TextareaAutosize from "react-textarea-autosize";
+import { CiEdit } from "react-icons/ci";
+import { MdDeleteOutline } from "react-icons/md";
 
 const ReplyList = ({ pid }) => {
+  const login = sessionStorage.getItem("email");
   const db = getFirestore(app);
   const [list, setList] = useState([]);
 
@@ -26,25 +33,136 @@ const ReplyList = ({ pid }) => {
         rows.push({ id: row.id, ...row.data() });
       });
       console.log(rows);
-      setList(rows);
+      const data = rows.map(
+        (row) =>
+          row && { ...row, ellipsis: true, edit: false, text: row.contents }
+      );
+      setList(data);
     });
+  };
+
+  const onClickContents = (id) => {
+    const data = list.map((reply) =>
+      reply.id === id ? { ...reply, ellipsis: !reply.ellipsis } : reply
+    );
+    setList(data);
+  };
+
+  const onClickUpdate = (id) => {
+    const data = list.map((reply) =>
+      reply.id === id ? { ...reply, edit: !reply.edit } : reply
+    );
+    console.log(id);
+    setList(data);
+  };
+
+  const onChangeContents = (id, e) => {
+    const data = list.map((reply) =>
+      reply.id === id ? { ...reply, contents: e.target.value } : reply
+    );
+    setList(data);
+  };
+
+  const onClickCancel = (r) => {
+    const data = list.map((reply) =>
+      reply.id === r.id
+        ? { ...reply, edit: false, contents: reply.text }
+        : reply
+    );
+    setList(data);
+  };
+
+  const onClickSave = async (reply) => {
+    try {
+      const docRef = doc(db, "reply", reply.id);
+      await updateDoc(docRef, {
+        contents: reply.contents,
+      });
+
+      const data = list.map((r) =>
+        r.id === reply.id ? { ...r, edit: false, text: reply.contents } : r
+      );
+      setList(data);
+    } catch (error) {
+      console.error("update on error:", error);
+      alert("update on error");
+    }
+  };
+
+  const onClickDelete = async (replyId) => {
+    if (window.confirm("If you want to delete this comment, please click OK")) {
+      try {
+        const docRef = doc(db, "reply", replyId);
+        await deleteDoc(docRef);
+        console.log("delete on success");
+      } catch (error) {
+        console.error("delete on error:", error);
+        alert("delete on error");
+      }
+    }
   };
 
   useEffect(() => {
     getList();
   }, []);
+
   return (
-    <Row className="my-2">
-      <Col md={10} className="mx-auto">
-        <h3>Comment Lists</h3>
+    <Row className="justify-content-center">
+      <Col md={10}>
         {list.map((reply) => (
-          <div key={reply.id}>
-            <div>
-              {reply.date} : {reply.email}
-            </div>
-            <div>
-              <p style={{ whiteSpace: "pre-wrap" }}>{reply.contents}</p>
-            </div>
+          <div key={reply.id} className="my-5">
+            <Row>
+              <Col className="text-muted">
+                {reply.date} : {reply.email}
+              </Col>
+              {reply.email === login && !reply.edit && (
+                <Col className="text-end">
+                  <CiEdit
+                    onClick={() => onClickUpdate(reply.id)}
+                    className="edit"
+                  />
+                  <MdDeleteOutline
+                    onClick={() => onClickDelete(reply.id)}
+                    className="delete"
+                  />
+                </Col>
+              )}
+            </Row>
+            {reply.edit ? (
+              <Form>
+                <TextareaAutosize
+                  className="textarea"
+                  onChange={(e) => onChangeContents(reply.id, e)}
+                  value={reply.contents}
+                />
+                <div className="text-end">
+                  <Button
+                    size="sm"
+                    variant="primary"
+                    className="mx-2"
+                    disabled={reply.text === reply.contents}
+                    onClick={() => onClickSave(reply)}
+                  >
+                    저장
+                  </Button>
+                  <Button
+                    onClick={() => onClickCancel(reply)}
+                    size="sm"
+                    variant="secondary"
+                  >
+                    취소
+                  </Button>
+                </div>
+              </Form>
+            ) : (
+              <div
+                onClick={() => onClickContents(reply.id)}
+                style={{ cursor: "pointer" }}
+                className={reply.ellipsis ? "cmt-toggle" : ""}
+              >
+                <p style={{ whiteSpace: "pre-wrap" }}>{reply.contents}</p>
+              </div>
+            )}
           </div>
         ))}
       </Col>
